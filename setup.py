b0623bc87676be643003c235d1ea545e6876404c
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2022-02-19 19:27:11 +0000

    removed the apache licese from setup

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
