f46da3672d109024b9a05a9e22941e8f6db1a3fe
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2022-06-09 11:29:00 +0100

    Added extra import check workflow

---
+++ tools/check.import.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+__check(){
+    grep -r -e "^import" -e "^from" TermTk |
+        grep -v -e "from TermTk" -e "import TermTk" |
+        grep -v "__init__.py:from \.[^ ]* *import" |
+        grep -v -e "import re" -e "import os" -e "import datetime" |
+        grep -v \
+            -e "from dataclasses" \
+            -e "timer.py:import threading, time" \
+            -e "log.py:import inspect" \
+            -e "log.py:import logging" \
+            -e "log.py:from collections.abc import Callable, Set" \
+            -e "from time" -e "input.py:import platform" \
+            -e "readinputlinux.py:import sys, os, select" \
+            -e "term.py:import sys, os, signal" \
+            -e "ttk.py:import signal" \
+            -e "ttk.py:import time" \
+            -e "ttk.py:import queue" \
+            -e "filebuffer.py:import threading"
+} ;
+
+if __check ;  then
+    echo "Failed Dependencies verification!!!" ;
+    echo "Please check:" ;
+    echo "#######################"
+    __check ;
+    echo "#######################"
+    exit 1 ;
+else
+    echo "Dependencies Verified!!!"
+    exit 0 ;
+fi ;
