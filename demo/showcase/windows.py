7a7226de87943f7a3e4aa17f29d3b4b53800c0b8
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2022-07-24 11:55:35 +0100

    added logviewer in the windows showcase

--- demo/showcase/windows.py
+++ demo/showcase/windows.py
@@ -32,19 +32,19 @@ import TermTk as ttk
 def demoWindows(root=None):
     frame = ttk.TTkFrame(parent=root, border=False)
 
-    win2_1 = ttk.TTkWindow(parent=frame,pos = (0,0), size=(50,25), title="Test Window 2.1", border=True)
+    win2_1 = ttk.TTkWindow(parent=frame,pos = (0,0), size=(50,30), title="Test Window 1", border=True)
     win2_1.setLayout(ttk.TTkHBoxLayout())
     ttk.TTkTestWidget(parent=win2_1, border=False)
 
-    win2_2 = ttk.TTkWindow(parent=frame,pos = (23,4), size=(52,20), title="Test Window 2.2", border=True)
+    win2_2 = ttk.TTkWindow(parent=frame,pos = (15,4), size=(87,20), title="Log Window", border=True)
     win2_2.setLayout(ttk.TTkHBoxLayout())
-    ttk.TTkTestWidget(parent=win2_2, border=False)
+    ttk.TTkLogViewer(parent=win2_2, follow=True )
 
-    win2_3 = ttk.TTkWindow(parent=frame,pos = (6,21), size=(80,7), title="Captured Input", border=True)
+    win2_3 = ttk.TTkWindow(parent=frame,pos = (5,25), size=(85,7), title="Captured Input", border=True)
     win2_3.setLayout(ttk.TTkHBoxLayout())
     ttk.TTkKeyPressView(parent=win2_3)
 
-    ttk.TTkAbout(parent=frame, pos=(11,7))
+    ttk.TTkAbout(parent=frame, pos=(5,7))
 
     return frame
 
