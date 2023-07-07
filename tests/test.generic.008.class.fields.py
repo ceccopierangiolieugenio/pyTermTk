2a345d552d2ca6068f938fcb385beb47f99aed16
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2023-07-07 11:25:24 +0100

    Added generic class test

---
+++ tests/test.generic.008.class.fields.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2023 Eugenio Parodi <ceccopierangiolieugenio AT googlemail DOT com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+class A():
+    style = {'a':123, 'b':123}
+    def __init__(self) -> None:
+        print(f"{self.__class__.__name__} : {self.style=} , {A.style=}")
+
+class B(A):
+    style = A.style | {'b':456, 'c':456}
+    def __init__(self) -> None:
+        print(f"{self.__class__.__name__} : {self.style=} , {B.style=}")
+
+class C(B):
+    style = B.style | {'a':789, 'c':789}
+    def __init__(self) -> None:
+        print(f"{self.__class__.__name__} : {self.style=} , {C.style=}, {style=}")
+
+a = A()
+b = B()
+c = C()
