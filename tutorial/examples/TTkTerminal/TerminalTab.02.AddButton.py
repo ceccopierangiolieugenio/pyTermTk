9b2b0665a9f8e4360d50865004b114fa17f1429e
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2023-09-27 11:43:05 +0100

    Improved the comments in the Terminal Examples

--- tutorial/examples/TTkTerminal/TerminalTab.02.AddButton.py
+++ tutorial/examples/TTkTerminal/TerminalTab.02.AddButton.py
@@ -30,10 +30,10 @@ sys.path.append(os.path.join(sys.path[0],'../../..'))
 import TermTk as ttk
 
 # layout = GridLayout
-#   It is required to allow the tab to be automatically resized to the "root" area
+#   It is required to allow the tabWidget to be automatically resized to the "root" area
 # mouseTrack = True (optional)
-#   It is required if we want to forward the mouse move events to the terminals
-#   i.e. the mouse drag feature of vim
+#   It is required if we want to forward the mouse over events to the terminals
+#   i.e. the mouse over feature of pytermTk or Textual
 root = ttk.TTk(layout=ttk.TTkGridLayout(), mouseTrack=True)
 
 tab = ttk.TTkTabWidget(parent=root)
