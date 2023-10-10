14536dde1850c1dbabe507127d75819fae676a53
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2023-10-10 11:36:30 +0100

    changed demo list

--- demo/showcase/list.py
+++ demo/showcase/list.py
@@ -44,12 +44,12 @@ def demoList(root= None):
     layout1 = ttk.TTkLayout()
 
     # Place the widgets in the root layout
-    rootLayout.addWidget(win1,0,0)
-    rootLayout.addWidget(win2,0,1)
-    rootLayout.addWidget(win3,0,2,1,3)
-    rootLayout.addItem(layout1,1,0,1,3)
-    rootLayout.addWidget(win4,1,3)
-    rootLayout.addWidget(win5,1,4)
+    rootLayout.addWidget(win1,0,0,2,1)
+    rootLayout.addWidget(win2,0,1,2,1)
+    rootLayout.addWidget(win3,0,2,2,3)
+    rootLayout.addItem(layout1,2,0,1,3)
+    rootLayout.addWidget(win4,2,3)
+    rootLayout.addWidget(win5,2,4)
 
     # Single Selection List
     listWidgetSingle = ttk.TTkList(parent=win1, maxWidth=40, minWidth=10)
