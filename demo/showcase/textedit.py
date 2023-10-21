2f9b1251d713af75f883814cf2a8f79bae296202
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2023-10-21 14:57:50 +0100

    Enhancement: #189 allow to set the starting value of the line number in the Textedit

--- demo/showcase/textedit.py
+++ demo/showcase/textedit.py
@@ -52,7 +52,7 @@ def demoTextEdit(root=None, document=None):
     # If no document is passed a default one is created,
     # In this showcase I want to be able to share the same
     # document among 2 textEdit widgets
-    te = ttk.TTkTextEdit(document=document, lineNumber=True)
+    te = ttk.TTkTextEdit(document=document, lineNumber=True, lineNumberStarting=1)
 
     te.setReadOnly(False)
 
@@ -127,7 +127,7 @@ def demoTextEdit(root=None, document=None):
     # Empty columns/cells are 1 char wide due to "columnMinWidth=1" parameter in the GridLayout
     #           1       3                    8                11
     #    0       2       4    5    6    7     9       10       12
-    # 0  [ ] FG  [ ] BG  [ ] LineNumber
+    # 0  [ ] FG  [ ] BG  [ ] LineNumber [  0]Starting Number
     # 1  ┌─────┐ ┌─────┐ ╒═══╕╒═══╕╒═══╕╒═══╕ ┌──────┐┌──────┐
     # 2  │     │ │     │ │ a ││ a ││ a ││ a │ │ UNDO ││ REDO │
     # 3  └─────┘ └─────┘ └───┘└───┘└───┘└───┘ ╘══════╛└──────┘ ┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
@@ -140,6 +140,7 @@ def demoTextEdit(root=None, document=None):
     fontLayout.addWidget(btn_bgColor := ttk.TTkColorButtonPicker(border=True, enabled=False, maxSize=(7   ,3)),1,2)
 
     fontLayout.addWidget(cb_linenumber := ttk.TTkCheckbox(text=" LineNumber", checked=True),0,4,1,3)
+    fontLayout.addWidget(sb_linenumber := ttk.TTkSpinBox(value=1, maxWidth=5, maximum=10000, minimum=-10000, enabled=True),0,7,1,1)
 
     # Char style buttons
     fontLayout.addWidget(btn_bold          := ttk.TTkButton(border=True, maxSize=(5,3), checkable=True, text=ttk.TTkString( 'a' , ttk.TTkColor.BOLD)        ),1,4)
@@ -209,7 +210,9 @@ def demoTextEdit(root=None, document=None):
     cb_fg.clicked.connect(lambda _: _setStyle())
     cb_bg.clicked.connect(lambda _: _setStyle())
 
-    cb_linenumber.stateChanged.connect(lambda x: te.setLineNumber(x==ttk.TTkK.Checked))
+    cb_linenumber.toggled.connect(te.setLineNumber)
+    cb_linenumber.toggled.connect(sb_linenumber.setEnabled)
+    sb_linenumber.valueChanged.connect(te.setLineNumberStarting)
 
     btn_fgColor.colorSelected.connect(lambda _: _setStyle())
     btn_bgColor.colorSelected.connect(lambda _: _setStyle())
