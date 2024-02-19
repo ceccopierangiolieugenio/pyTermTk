d2413efafb171f0ce5b340a47d958971d75dd465
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2024-02-19 22:55:10 +0000

    Added Licence in the apptemplate

--- demo/showcase/apptemplate.py
+++ demo/showcase/apptemplate.py
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright (c) 2021 Eugenio Parodi <ceccopierangiolieugenio AT googlemail DOT com>
+# Copyright (c) 2024 Eugenio Parodi <ceccopierangiolieugenio AT googlemail DOT com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
