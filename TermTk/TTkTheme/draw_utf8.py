9c1fad229c86a69e53714c05840a6ed91225cb37
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2024-11-16 13:42:25 +0000

    Added missing __all__

--- TermTk/TTkTheme/draw_utf8.py
+++ TermTk/TTkTheme/draw_utf8.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+__all__ = ['TTkTheme']
+
 class TTkTheme():
     ''' from: https://en.wikipedia.org/wiki/Box-drawing_character
 
