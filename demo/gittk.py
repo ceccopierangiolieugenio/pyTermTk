8130c6c8347ce9fdf31070223bbcff347d995435
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2025-03-05 16:06:53 +0000

    feat: added file browse in gittk

--- demo/gittk.py
+++ demo/gittk.py
@@ -30,77 +30,124 @@ import TermTk as ttk
 
 class GitHelper():
     def graph():
-        return subprocess.run(['git',"log", "--pretty=format:---CC%hCC--- ---DD%adDD--- ---MM%s%dMM--- ---AA%anAA---", "--graph", "--date=short", "--color=always"],capture_output=True).stdout.decode('utf-8').split('\n')
+        return subprocess.run(['git',"log", "--all", "--pretty=format:---CC%hCC--- ---DD%adDD--- ---MM%s%dMM--- ---AA%anAA---", "--graph", "--date=short", "--color=always"],capture_output=True).stdout.decode('utf-8').split('\n')
 
-    def show(commit):
+    def show(commit) -> str:
         return subprocess.run(['git',"show", commit, "--color=always"],capture_output=True).stdout.decode('utf-8')
 
+    def fileList(commit) -> str:
+        return subprocess.run(['git',"show", "--name-only", "--pretty=", commit],capture_output=True).stdout.decode('utf-8')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', help='Full Screen', action='store_true')
-args = parser.parse_args()
+class GitTTK(ttk.TTkAppTemplate):
 
-fullscreen = args.f
-allCommits = []
-commitResults = []
-minGraph = 0
-minAuthor = 0
-minDate = 0
-reGraph   = re.compile(r'^(.*)---CC.*$')
-reTime    = re.compile(r'^.*---DD(.*)DD---.*$')
-reMessage = re.compile(r'^.*---MM(.*)MM---.*$')
-reAuthor  = re.compile(r'^.*---AA(.*)AA---.*$')
-reCommit  = re.compile(r'^.*---CC(.*)CC---.*$')
-for line in GitHelper.graph():
-    g = _m.group(1) if (_m:=reGraph.match(line))   else line
-    g = ttk.TTkString(g)
-    minGraph = max(minGraph, g.termWidth())
-    d = _m.group(1) if (_m:=reTime.match(line))    else ""
-    minDate = max(minDate, len(d))
-    a = _m.group(1) if (_m:=reAuthor.match(line))  else ""
-    minAuthor = max(minAuthor, len(a))
-    m = _m.group(1) if (_m:=reMessage.match(line)) else ""
-    c = _m.group(1) if (_m:=reCommit.match(line))  else ""
-    cc = f"{c} - {m}" if c and m else ""
-    allCommits.append(c)
-    commitResults.append((g, cc, a, d))
+    def __init__(self, border=False, **kwargs):
+        self._allCommits = []
+        self._commitResults = []
+        self._diffLines = []
 
+        super().__init__(border, **kwargs)
 
-root = ttk.TTk()
-if fullscreen:
-    gittk = root
-    root.setLayout(ttk.TTkGridLayout())
-else:
+        self._tableCommit = ttk.TTkFancyTable(selectColor=ttk.TTkColor.bg('#882200'))
+        self._diffText:ttk.TTkTextEditView = ttk.TTkTextEdit(readOnly=False)
+        self._fileList:ttk.TTkListWidget = ttk.TTkList()
+        self._logView = ttk.TTkLogViewer()
+
+        w,h = self.size()
+
+        self.setWidget(widget=self._tableCommit, title="Diff",  size=h//2, position=ttk.TTkAppTemplate.HEADER)
+        self.setWidget(widget=self._diffText,    title="Diff",             position=ttk.TTkAppTemplate.MAIN)
+        self.setWidget(widget=self._fileList,    title="Files", size=50,   position=ttk.TTkAppTemplate.RIGHT)
+        self.setWidget(widget=self._logView,     title="Logs",  size=0,    position=ttk.TTkAppTemplate.FOOTER)
+
+        self._refreshCommits()
+
+        @ttk.pyTTkSlot(int)
+        def _tableCallback(val):
+            commit = self._allCommits[val]
+            if not commit: return
+            diff = GitHelper.show(commit).replace(chr(65039),' ')
+            self._diffText.setText(diff)
+            self._diffLines = diff.split('\n')
+            files = GitHelper.fileList(commit).split('\n')
+            files = [l for l in files if l] # remove empty lines
+            self._fileList.removeItems(self._fileList.items())
+            self._fileList.addItem('Comment')
+            self._fileList.addItems(files)
+
+        self._tableCommit.activated.connect(_tableCallback)
+        self._fileList.textClicked.connect(self._selectFileCallback)
+
+    def _refreshCommits(self):
+        minGraph = 0
+        minAuthor = 0
+        minDate = 0
+        reGraph   = re.compile(r'^(.*)---CC.*$')
+        reTime    = re.compile(r'^.*---DD(.*)DD---.*$')
+        reMessage = re.compile(r'^.*---MM(.*)MM---.*$')
+        reAuthor  = re.compile(r'^.*---AA(.*)AA---.*$')
+        reCommit  = re.compile(r'^.*---CC(.*)CC---.*$')
+
+        for line in GitHelper.graph():
+            g = _m.group(1) if (_m:=reGraph.match(line))   else line
+            g = ttk.TTkString(g)
+            minGraph = max(minGraph, g.termWidth())
+            d = _m.group(1) if (_m:=reTime.match(line))    else ""
+            minDate = max(minDate, len(d))
+            a = _m.group(1) if (_m:=reAuthor.match(line))  else ""
+            minAuthor = max(minAuthor, len(a))
+            m = _m.group(1) if (_m:=reMessage.match(line)) else ""
+            c = _m.group(1) if (_m:=reCommit.match(line))  else ""
+            cc = f"{c} - {m}" if c and m else ""
+            self._allCommits.append(c)
+            self._commitResults.append((g, cc, a, d))
+
+        self._tableCommit.setHeader(("","commit","Name","Date"))
+        self._tableCommit.setColumnSize((minGraph,-1,minAuthor+1,15))
+        self._tableCommit.setColumnColors((
+                ttk.TTkColor.RST,
+                ttk.TTkColor.fg('#cccccc', modifier=ttk.TTkColorGradient(increment=-2)),
+                ttk.TTkColor.fg('#888800', modifier=ttk.TTkColorGradient(increment=6)),
+                ttk.TTkColor.fg('#00dddd', modifier=ttk.TTkColorGradient(increment=-4)),
+            ))
+        for commit in self._commitResults:
+            self._tableCommit.appendItem(commit)
+
+    ttk.pyTTkSlot(str)
+    def _selectFileCallback(self, file):
+        # cursor = diffText.textCursor()
+        # cursor.
+        self._diffText.textCursor().movePosition(operation=ttk.TTkTextCursor.MoveOperation.End)
+        self._diffText.ensureCursorVisible()
+        if file == 'Comment':
+            self._diffText.textCursor().setPosition(line=0,pos=0)
+            self._diffText.setExtraSelections([])
+        elif self._diffText.find(file):
+            cursor = self._diffText.textCursor().copy()
+            cursor.clearSelection()
+            selection = ttk.TTkTextEdit.ExtraSelection(
+                                            cursor=cursor,
+                                            color=ttk.TTkColor.BG_RED,
+                                            format=ttk.TTkK.SelectionFormat.FullWidthSelection)
+            self._diffText.setExtraSelections([selection])
+        self._diffText.ensureCursorVisible()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', help='Windowed', action='store_true')
+    args = parser.parse_args()
+
+    fullscreen = not args.w
+
     root = ttk.TTk()
-    gittk = ttk.TTkWindow(parent=root,pos = (1,1), size=(100,40), title="gittk", border=True, layout=ttk.TTkGridLayout())
+    if fullscreen:
+        gittk = root
+        root.setLayout(ttk.TTkGridLayout())
+    else:
+        root = ttk.TTk()
+        gittk = ttk.TTkWindow(parent=root,pos = (1,1), size=(100,40), title="gittk", border=True, layout=ttk.TTkGridLayout())
+    GitTTK(parent=gittk)
 
-gittkVsplitter = ttk.TTkSplitter(parent=gittk, orientation=ttk.TTkK.VERTICAL)
-tableCommit = ttk.TTkFancyTable(parent=gittkVsplitter, selectColor=ttk.TTkColor.bg('#882200'))
-gittkHsplitter = ttk.TTkSplitter(parent=gittkVsplitter, orientation=ttk.TTkK.HORIZONTAL)
-diffText = ttk.TTkTextEdit(parent=gittkHsplitter)
-gittkHsplitter.addWidget(ttk.TTkTestWidgetSizes(border=True, title="Details"),20)
-gittkVsplitter.addWidget(ttk.TTkLogViewer(),3)
+    root.mainloop()
 
-tableCommit.setColumnSize((minGraph,-1,minAuthor+1,15))
-
-tableCommit.setHeader(("","commit","Name","Date"))
-tableCommit.setColumnColors((
-        ttk.TTkColor.RST,
-        ttk.TTkColor.fg('#cccccc', modifier=ttk.TTkColorGradient(increment=-2)),
-        ttk.TTkColor.fg('#888800', modifier=ttk.TTkColorGradient(increment=6)),
-        ttk.TTkColor.fg('#00dddd', modifier=ttk.TTkColorGradient(increment=-4)),
-    ))
-
-for commit in commitResults:
-    tableCommit.appendItem(commit)
-
-@ttk.pyTTkSlot(int)
-def _tableCallback(val):
-    commit = allCommits[val]
-    if not commit: return
-    diff = GitHelper.show(commit)
-    diffText.setText(diff)
-
-tableCommit.activated.connect(_tableCallback)
-
-root.mainloop()
+if __name__ == "__main__":
+    main()
