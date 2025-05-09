6a5fd16ba723669e92e5783d1cd05cf2b5529139
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2025-05-09 14:18:13 +0100

    refactor: move the main routine outside the a folder

--- apps/tlogg/tlogg/helper.py
+++ apps/tlogg/tlogg/helper.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+__all__ = ['TloggHelper']
+
 import importlib, pkgutil
 import runpy
 import sys, os
