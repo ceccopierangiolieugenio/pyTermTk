94f1fc0e3c4a4c6824f70d6cca17ce179e68059a
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2025-06-03 17:57:03 +0100

    chore: removed unrequired methods

--- tools/autogenForwarded.py
+++ tools/autogenForwarded.py
@@ -63,102 +63,14 @@ def write_lines_to_file(lines: List[str], output_path: str) -> bool:
         print(f"Error: An error occurred while writing to the file: {e}")
         return False
 
-def find_method_origin(cls, method_name: str) -> type | None:
-    """
-    Finds the class in which a method is originally defined in a class hierarchy.
-
-    Args:
-        cls: The class to start searching from.
-        method_name: The name of the method to find.
-
-    Returns:
-        The class in which the method is originally defined, or None if the method is not found
-        in the class hierarchy.
-    """
-    for base in inspect.getmro(cls):
-        if method_name in base.__dict__:
-            return base
-    return None
-
-def get_field_docstring(cls, field_name: str) -> str | None:
-    """
-    Retrieves the docstring of a field (attribute) in a class.
-
-    This function assumes the docstring is defined directly after the field
-    declaration in the class definition.
-
-    Args:
-        cls: The class object.
-        field_name: The name of the field.
-
-    Returns:
-        The docstring of the field, or None if the field or docstring is not found.
-    """
-    try:
-
-        # Get the source code of the class
-        source = inspect.getsource(cls)
-
-        # Construct a regex to find the field and its docstring
-        pattern = rf'\s*{field_name}:.*\n\s*"""(.*?)"""'
-
-        # Search for the pattern in the source code
-        match = re.search(pattern, source, re.DOTALL)  # re.DOTALL allows . to match newlines
-
-        if match:
-            return match.group(1).strip()  # Return the docstring, stripping whitespace
-        else:
-            print(f"Docstring not found for field '{field_name}' in class '{cls.__name__}'.")
-            return None
-
-    except AttributeError:
-        print(f"Error: Field '{field_name}' not found in class '{cls.__name__}'.")
-        return None
-    except OSError:
-        print(f"Error: Could not retrieve source code for class '{cls.__name__}'.")
-        return None
-    except Exception as e:
-        print(f"Error: An unexpected error occurred: {e}")
-        return None
-
-
 def _index_of(_m:str, lines:List[str]) -> int:
     for i,l in enumerate(lines):
         if _m in l:
             return i
     raise ValueError(f"End Delimiter '{_m}' not found in the filtered lines")
 
-marker_params = "#--FORWARD-AUTOGEN-PARAMS--#"
 marker_start = "#--FORWARD-AUTOGEN-START--#"
 marker_end = "#--FORWARD-AUTOGEN-END--#"
-
-def extract_and_process_lines(lines: List[str]) -> Dict:
-    """
-    Extracts lines between '#--START-FORWARD-AUTOGEN--#' and '#--END--#' delimiters,
-    uncomments them, and removes the slice after the '#' character.
-
-    Args:
-        lines (List[str]): A list of strings representing the lines of a file.
-    """
-
-    index_params = _index_of(marker_params,lines)
-    index_start = _index_of(marker_start,lines)
-    index_end = _index_of(marker_end,lines)
-
-    param_lines: List[str] = lines[index_params+1:index_start]
-    extracted_lines: List[str] = []
-    lines[index_start+1:index_end] = []
-
-    content_re = re.compile(r'^ *#([^#]*)(?:#.*)?$')
-    for line in param_lines:
-        if _m:=content_re.match(line):
-            uncommented_line = _m.group(1)
-            extracted_lines.append(uncommented_line)
-
-    if not extracted_lines:
-        return {}
-
-    return json.loads(''.join(extracted_lines))
 
 from TermTk.TTkAbstract.abstractscrollarea import _ForwardData
 def autogen_methods(data: _ForwardData) -> List[str]:
@@ -217,48 +129,6 @@ def autogen_methods(data: _ForwardData) -> List[str]:
             print(f"Error: Method '{method_name}' not found in class '{class_name}'.")
         except Exception as e:
             print(f"Error: An error occurred while processing method '{method_name}': {e}")
-
-    return signatures
-
-
-def autogen_signals(data: Dict[str, Any]) -> List[str]:
-    """
-    Generates a list of signal signatures and return types for a given class.
-
-    Args:
-        data (Dict[str, Any]): A dictionary containing the class name and a list of signals.
-            Example: {'signals': 'TTkTextedit._signals'}
-
-    Returns:
-        List[str]: A list of strings, where each string is a method signature and return type.
-    """
-    import TermTk as ttk
-
-    if not 'signals' in data:
-        raise ValueError('"sigals" not in data')
-
-    class_name,signals_list_name = data['signals'].split('.')
-    signatures: List[str] = []
-
-    try:
-        # Get the class from the ttk module
-        cls = getattr(ttk, class_name)
-        signals = getattr(cls, signals_list_name)
-        cls = getattr(ttk, data['class'])
-    except AttributeError:
-        print(f"Error: Class '{class_name}' not found in TermTk module.")
-        return []
-
-    for signal_name in signals:
-        try:
-            # Get the method from the class
-            signal = getattr(cls, signal_name)
-            doc = get_field_docstring(cls, signal_name)
-            print(doc)
-        except AttributeError:
-            print(f"Error: Signal '{signal_name}' not found in class '{class_name}'.")
-        except Exception as e:
-            print(f"Error: An error occurred while processing signal '{signal_name}': {e}")
 
     return signatures
 
