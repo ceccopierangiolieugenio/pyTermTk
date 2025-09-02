f3e21b9757724012456abfd2cc2e072c8092c12f
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2025-09-02 09:18:21 +0100

    docs: fix doc gen

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../libs/pyTermTk'))
 sys.path.insert(0, os.path.abspath('sphinx_modules'))
 
 import TermTk
