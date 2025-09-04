f69edafb986d71b46e57b9cfc88ede001c2ae8a9
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2025-09-04 23:23:12 +0100

    chore revert scratchpad

--- tools/ci/ci_tools/social/notify_bluesky.py
+++ tools/ci/ci_tools/social/notify_bluesky.py
@@ -36,69 +36,6 @@ from social_common import get_social_data, SocialData, get_env_var
 import re
 from typing import List, Dict
 
-def _parse_mentions(text: str) -> List[Dict]:
-    spans = []
-    # regex based on: https://atproto.com/specs/handle#handle-identifier-syntax
-    mention_regex = rb"[$|\W](@([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)"
-    text_bytes = text.encode("UTF-8")
-    for m in re.finditer(mention_regex, text_bytes):
-        spans.append({
-            "start": m.start(1),
-            "end": m.end(1),
-            "handle": m.group(1)[1:].decode("UTF-8")
-        })
-    return spans
-
-def _parse_urls(text: str) -> List[Dict]:
-    spans = []
-    # partial/naive URL regex based on: https://stackoverflow.com/a/3809435
-    # tweaked to disallow some training punctuation
-    url_regex = rb"[$|\W](https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*[-a-zA-Z0-9@%_\+~#//=])?)"
-    text_bytes = text.encode("UTF-8")
-    for m in re.finditer(url_regex, text_bytes):
-        spans.append({
-            "start": m.start(1),
-            "end": m.end(1),
-            "url": m.group(1).decode("UTF-8"),
-        })
-    return spans
-
-# Parse facets from text and resolve the handles to DIDs
-def _parse_facets(text: str) -> List[Dict]:
-    facets = []
-    for m in _parse_mentions(text):
-        resp = requests.get(
-            "https://bsky.social/xrpc/com.atproto.identity.resolveHandle",
-            params={"handle": m["handle"]},
-        )
-        # If the handle can't be resolved, just skip it!
-        # It will be rendered as text in the post instead of a link
-        if resp.status_code == 400:
-            continue
-        did = resp.json()["did"]
-        facets.append({
-            "index": {
-                "byteStart": m["start"],
-                "byteEnd": m["end"],
-            },
-            "features": [{"$type": "app.bsky.richtext.facet#mention", "did": did}],
-        })
-    for u in _parse_urls(text):
-        facets.append({
-            "index": {
-                "byteStart": u["start"],
-                "byteEnd": u["end"],
-            },
-            "features": [
-                {
-                    "$type": "app.bsky.richtext.facet#link",
-                    # NOTE: URI ("I") not URL ("L")
-                    "uri": u["url"],
-                }
-            ],
-        })
-    return facets
-
 def _get_facet(txt:str, slice:str, content:Dict ) -> Dict:
     start = txt.index(slice)
     end = start + len(slice)
