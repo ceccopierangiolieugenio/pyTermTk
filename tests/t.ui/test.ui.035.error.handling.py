a3d4c2536f090c94877469391be2e0771c4cf01f
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2025-10-27 23:53:13 +0000

    chore: license

--- tests/t.ui/test.ui.035.error.handling.py
+++ tests/t.ui/test.ui.035.error.handling.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-
 # MIT License
 #
-# Copyright (c) 2021 Eugenio Parodi <ceccopierangiolieugenio AT googlemail DOT com>
+# Copyright (c) 2025 Eugenio Parodi <ceccopierangiolieugenio AT googlemail DOT com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
