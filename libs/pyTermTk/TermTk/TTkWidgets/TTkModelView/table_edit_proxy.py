9b9763eed3176dfcf383fdd479d6806b3d18a5e3
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2025-11-08 14:53:41 +0000

    chore: fix

--- libs/pyTermTk/TermTk/TTkWidgets/TTkModelView/table_edit_proxy.py
+++ libs/pyTermTk/TermTk/TTkWidgets/TTkModelView/table_edit_proxy.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2024 Eugenio Parodi <ceccopierangiolieugenio AT googlemail DOT com>
+# Copyright (c) 2025 Eugenio Parodi <ceccopierangiolieugenio AT googlemail DOT com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
