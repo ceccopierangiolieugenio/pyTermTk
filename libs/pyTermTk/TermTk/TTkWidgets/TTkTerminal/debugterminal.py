553bec963dd2ea469c9dbda3691a03b9d31cd190
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2025-12-22 20:35:36 +0100

    fix linter

--- libs/pyTermTk/TermTk/TTkWidgets/TTkTerminal/debugterminal.py
+++ libs/pyTermTk/TermTk/TTkWidgets/TTkTerminal/debugterminal.py
@@ -20,7 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__all__ = ['DebugTTkTerminal']
+# __all__ = ['DebugTTkTerminal']
+__all__ = []
 
 import os, pty, threading
 import struct, fcntl, termios
