9a7b70ab775feb911e64631ad94b6b6d0313e3af
Eugenio Parodi - Action <ceccopierangioliegenio@googlemail.com>
2026-03-03 17:59:08 +0000

    chore: updated TermTk and apps to versions

--- apps/tlogg/tlogg/__init__.py
+++ apps/tlogg/tlogg/__init__.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__:str = '0.48.1-a0'
+__version__:str = '0.49.0-a0'
 
 from .plugin import TloggPlugin
 from .proxy import tloggProxy, TloggViewerProxy
