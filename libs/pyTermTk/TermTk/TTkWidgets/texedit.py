3cebbf8cbb09115d0f15bc6ab7f48a807613ec08
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2026-04-13 14:35:58 +0100

    chore: fix autogen

--- libs/pyTermTk/TermTk/TTkWidgets/texedit.py
+++ libs/pyTermTk/TermTk/TTkWidgets/texedit.py
@@ -157,7 +157,7 @@ class TTkTextEdit(TTkAbstractScrollArea):
 
         This signal is emitted if the current character color has changed,
         for example caused by a change of the cursor position.
-
+        
         :param color: the new color
         :type color: :py:class:`TTkColor`
         '''
@@ -168,7 +168,7 @@ class TTkTextEdit(TTkAbstractScrollArea):
         .. seealso:: this method is forwarded to :py:meth:`TTkTextEditView.cursorPositionChanged`
 
         This signal is emitted whenever the position of the cursor changed.
-
+        
         :param cursor: the cursor changed.
         :type cursor: :py:class:`TTkTextCursor`
         '''
@@ -180,7 +180,7 @@ class TTkTextEdit(TTkAbstractScrollArea):
 
         This signal is emitted whenever undo operations become available (available is true)
         or unavailable (available is false).
-
+        
         :param available: the availability of undo
         :type available: bool
         '''
@@ -192,7 +192,7 @@ class TTkTextEdit(TTkAbstractScrollArea):
 
         This signal is emitted whenever redo operations become available (available is true)
         or unavailable (available is false).
-
+        
         :param available: the availability of redo
         :type available: bool
         '''
