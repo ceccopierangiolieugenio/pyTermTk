06bec2cb787c7bae207268b8783cde0f14f8ef80
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2026-05-15 11:06:08 +0100

    fix: specify type for __all__ as list[str] for better type hinting

--- libs/pyTermTk/TermTk/TTkGui/TTkTextWrap/text_wrap_engine_vim_wrap.py
+++ libs/pyTermTk/TermTk/TTkGui/TTkTextWrap/text_wrap_engine_vim_wrap.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__all__:list = []
+__all__:list[str] = []
 
 from dataclasses import dataclass
 from threading import RLock
