b8eeb1b5d94f104d7a6d338143db5631ce12c691
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2026-07-06 17:11:51 +0100

    fix: enable postponed evaluation of type annotations in canvas.py

--- libs/pyTermTk/TermTk/TTkCore/canvas.py
+++ libs/pyTermTk/TermTk/TTkCore/canvas.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from __future__ import annotations
+
 __all__ = ['TTkCanvas']
 
 from typing import List, Optional, Tuple, cast
