32f43c679cb014df19981447796fe6ee326cd930
Parodi, Eugenio 🌶 <ceccopierangiolieugenio@googlemail.com>
2026-07-23 14:51:17 +0100

    fix: remove unnecessary blank lines in TTkTerminal docstrings

--- libs/pyTermTk/TermTk/TTkWidgets/TTkTerminal/terminal.py
+++ libs/pyTermTk/TermTk/TTkWidgets/TTkTerminal/terminal.py
@@ -75,7 +75,7 @@ class TTkTerminal(TTkAbstractScrollArea):
         .. seealso:: this method is forwarded to :py:meth:`TTkTerminalView.titleChanged`
 
         This signal is emitted when the terminal title change through OSC "ESC ]0;"
-
+        
         :param title: the new title
         :type title: str
         '''
@@ -94,7 +94,7 @@ class TTkTerminal(TTkAbstractScrollArea):
         .. seealso:: this method is forwarded to :py:meth:`TTkTerminalView.textSelected`
 
         This signal is emitted when a text is selected.
-
+        
         :param text: the selected text
         :type text: :py:class:`ttkString`
         '''
@@ -105,14 +105,14 @@ class TTkTerminal(TTkAbstractScrollArea):
         .. seealso:: this method is forwarded to :py:meth:`TTkTerminalView.termData`
 
         This signal is emitted when data event fires.
-
+        
         This happens for example when the user types or pastes into the terminal.
         The event value is whatever 'str' results, in a typical setup,
         this should be passed on to the backing pty.
-
+        
         This signal is used in :py:class:`TTkTerminalHelper` through :py:meth:`TTkTerminalHelper.attachTTkTerminal`
         to frward all the terminal events to the pty interface.
-
+        
         :param data: the event data
         :type data: bytes
         '''
@@ -123,7 +123,7 @@ class TTkTerminal(TTkAbstractScrollArea):
         .. seealso:: this method is forwarded to :py:meth:`TTkTerminalView.termResized`
 
         This signal is emitted when the terminal is resized.
-
+        
         :param size: the new size [width, height] of the terminal
         :type size: (int,int)
         '''
